a218ddc4eb9f480b385193f143cabb7472901044
Omar Suriel <omarsuriel@icloud.com>
2022-09-14 16:08:01 -0400

    outfit-planner

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import clothing from './data';
+
 
 function App() {
+  const [dressCode, setDressCode] = useState('casual');
+
+  const findItem = (type) => {
+    const filteredItems = clothing.filter(
+      (clothingItem) =>
+        clothingItem.type === type && clothingItem.dressCode === dressCode
+      );
+
+      return filteredItems[Math.floor(Math.random() * filteredItems.length)];
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="header">
+        <div><h2>OutfitPlanner</h2></div>
+        <div><h2>Find what to wear</h2></div>
+        <div><h2>Fashion Style / Dress Code</h2></div>
+      </div>
+      <div  id="buttons-container">
+        <div style={{ marginBottom: '10px' }}>
+          <button onClick={() => setDressCode('casual')}>Casual</button>
+        </div>
+        <div style={{ marginBottom: '10px'  }}>
+          <button onClick={() => setDressCode('sport')}>Sport</button>
+          </div>
+        <div style={{ marginBottom: '10px' }}>
+          <button onClick={() => setDressCode('formal')}>Formal</button>
+        </div>
+      </div>
+      <div id="outfit-container">
+        <div>
+          <h3>Top</h3>
+          <div>
+            <img
+              src={findItem('top').imageUrl}
+              style={{ width: '120px', height: '120px', objectFit: 'cover' }}
+              alt="top item"
+            />
+          </div>
+        </div>
+        <div>
+          <h3>Bottom</h3>
+          <div>
+            <img
+              src={findItem('bottom').imageUrl}
+              style={{ width: '120px', height: '120px', objectFit: 'cover' }}
+              alt="Bottom item"
+            />
+          </div>
+        </div>
+        <div>
+          <h3>Shoes</h3>
+          <div>
+            <img src={findItem('shoes').imageUrl}  style={{ width: '120px', height: '120px', objectFit: 'cover' }} alt="shoes item"/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
